e4348d49d4257cb16d149d351f174bfe83e4bd91
shahin2525 <shahin451999@gmail.com>
2023-11-08 12:58:09 -0800

    problem-2 solve

--- index.ts
+++ index.ts
@@ -1,2 +1,43 @@
-const num1: number = 101;
-console.log(num1);
+// problem 1
+const getLengthOrSquare = (param: number | string) => {
+  if (typeof param === "string") {
+    return param.length;
+  } else if (typeof param === "number") {
+    return param * param;
+  }
+};
+
+getLengthOrSquare("rakib");
+
+// problem1
+
+// problem2
+interface Person {
+  name: string;
+  age: number;
+  address?: {
+    city: string;
+    street: string;
+  };
+  phone?: string;
+}
+
+const getAddressCity = (person: Person): string | undefined => {
+  if (person?.address?.city) {
+    return person.address.city;
+  } else {
+    undefined;
+  }
+};
+const person: Person = {
+  name: "rakib",
+  age: 25,
+  address: {
+    city: "dhaka",
+    street: "102",
+  },
+  phone: "Samsung",
+};
+console.log(getAddressCity(person));
+
+// problem2
